f79e0eb1733189960ea1e5ea41fdf1c77525a039
Louis <louis.haensch01@gmail.com>
2022-10-05 11:40:04 +0200

    Documentation improvements, graph now works

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-use std::{sync::Arc, fmt::Display, rc::Rc, cell::RefCell};
+use std::{fmt::Display, rc::Rc, cell::RefCell};
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
+/// An edge between two nodes inside the graph
 #[derive(Clone)]
 pub struct Edge<T: Display> {
     /// The "cost" of moving along this edge
@@ -15,7 +12,12 @@ pub struct Edge<T: Display> {
 }
 
 impl<T: Display> Edge<T> {
-    fn new(weight: i32, parent: Rc<RefCell<Node<T>>>, target: Rc<RefCell<Node<T>>>) -> Self {
+    /// Creates a new edge
+    /// # Params
+    /// - `weight` the weight of this edge
+    /// - `parent` the node from which the edge originates
+    /// - `target` the node to which the edge lands
+    pub fn new(weight: i32, parent: Rc<RefCell<Node<T>>>, target: Rc<RefCell<Node<T>>>) -> Self {
         Self {
             weight,
             parent,
@@ -24,6 +26,7 @@ impl<T: Display> Edge<T> {
     }
 }
 
+/// A node inside the graph
 #[derive(Clone)]
 pub struct Node<T: Display> {
     id: T,
@@ -37,54 +40,65 @@ impl<T: Display> Display for Node<T> {
 }
 
 impl<T: Display> Node<T> {
-    fn new(id: T) -> Self {
+    /// Creates a new node
+    /// `id` - An identifier for this node, should be unique
+    pub fn new(id: T) -> Self {
         Self {
             id,
             edges: Vec::new(),
         }
     }
 
-    /// Adds an edge to the node<
-    fn add_edge(&mut self, edge: Edge<T>) {
+    /// Adds an edge to the node
+    pub fn add_edge(&mut self, edge: Edge<T>) {
         self.edges.push(edge)
     }
 }
 
+/// Structure to organize nodes with edges
 pub struct Graph<T: Display> {
-    graphs: Vec<Vec<Rc<RefCell<Node<T>>>>>,
+    nodes: Vec<Rc<RefCell<Node<T>>>>,
 }
 
 impl<'a, T: Display + Clone> Graph<T> {
-    fn new() -> Self {
+    /// Creates a new graph
+    pub fn new() -> Self {
         Self {
-            graphs: Vec::new(),
+            nodes: Vec::new(),
         }
     }
 
-    fn add_node(&'a mut self, x: usize, y: usize, node: Node<T>) {
-        if self.graphs.get(y).is_none() {
-            self.graphs.push(Vec::new());
-        }
-        self.graphs[y][x] = Rc::new(RefCell::new(node))
+    /// Adds a new edge to the graph and returns the index of the node
+    pub fn add_node(&'a mut self, node: Node<T>) -> usize {
+        self.nodes.push(Rc::new(RefCell::new(node)));
+        self.nodes.len()-1
     }
 
-    fn add_edge(&mut self, parent_coordinates: (usize, usize), target_coordinates: (usize, usize)) {
-        let parent = Rc::new(RefCell::clone(&self.graphs.get(parent_coordinates.1).unwrap().get(parent_coordinates.0).unwrap()));
-        let target = Rc::new(RefCell::clone(&self.graphs.get(target_coordinates.1).unwrap().get(target_coordinates.0).unwrap()));
-        self.graphs[parent_coordinates.1][parent_coordinates.0].borrow_mut().add_edge(Edge::new(1, parent, target));
+    /// Adds an edge between two nodes, single direction
+    pub fn add_edge(&mut self, weight: i32, parent_index: usize, target_index: usize) {
+        let parent = Rc::new(RefCell::clone(&self.nodes.get(parent_index).unwrap()));
+        let target = Rc::new(RefCell::clone(&self.nodes.get(target_index).unwrap()));
+        self.nodes[parent_index].borrow_mut().add_edge(Edge::new(weight, parent, target));
+    }
+    
+    /// Adds an edge between two nodes, booth directions
+    pub fn add_double_edge(&mut self, weight: i32, parent_index: usize, target_index: usize) {
+        let parent = Rc::new(RefCell::clone(&self.nodes.get(parent_index).unwrap()));
+        let target = Rc::new(RefCell::clone(&self.nodes.get(target_index).unwrap()));
+        self.nodes[parent_index].borrow_mut().add_edge(Edge::new(weight, parent.clone(), target.clone()));
+        self.nodes[target_index].borrow_mut().add_edge(Edge::new(weight, target, parent));
     }
 }
 
 impl<T: Display> Display for Graph<T> {
+    /// Formats the graph to show all edges between nodes
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut graph = String::new();
-        for y in &self.graphs {
-            for x in y {
-                for edge in &x.borrow().edges {
-                    graph.push_str(&format!("{} -> {}", edge.parent.borrow().id, edge.target.borrow().id));
-                }
-                graph.push('\n');
+        for node in &self.nodes {
+            for edge in &node.borrow().edges {
+                graph.push_str(&format!("{} --{}--> {}", edge.parent.borrow().id, edge.weight, edge.target.borrow().id));
             }
+            graph.push('\n');
         }
         write!(f, "{}", graph)
     }
@@ -95,17 +109,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn some_test() {
         let mut graph = Graph::new();
-        graph.add_node(0, 0, Node::new(1));
-        graph.add_node(0, 1, Node::new(2));
-        graph.add_edge((0,0), (0,1));
+        graph.add_node(Node::new(1));
+        graph.add_node(Node::new(2));
+        graph.add_node(Node::new(3));
+        graph.add_edge(5, 0, 1);
+        graph.add_double_edge(10, 1, 2);
         println!("{}", graph);
     }
 }
